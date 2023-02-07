c57a04c348eb248c54c1b23ed4c3450e2f96f408
sdeix <sdeix2765@gmail.com>
2023-02-07 22:12:42 +0700

    создание начальной структуры

--- js/main.js
+++ js/main.js
@@ -1,90 +1,35 @@
-Vue.component('task',{
-        template:`
-<div class="task">
-    <input type="checkbox" id="checkbox" v-model="checked">
-    <label :class="{ taskcomplete: checked }" for="checkbox">{{name}} {{title}}</label>
+let eventBus = new Vue()
+
+
+Vue.component('board',{
+template:`
+<div id=board>
+      <div class="column column1"><card></card></div>
+      <div class="column column2"></div>
+      <div class="column column3"></div>
+
+
+
 </div>
+`
 
+})
 
-`,
-data() {
-       return {
-       checked:false
-       }
-   },
-computed: {
-       title() {
-           return this.checked;
-       }
-},
-props: {
-    name: String,
-    completed:Boolean,
-},
-
-watch: {
-    title(newValue, oldValue) {
-      this.$emit('updateParent', {
-        name: this.name,
-        completed: this.checked
+Vue.component('card',{
+      template:`
+      <div id=card>
+                  <p>qwew</p>
+      
+      
+      
+      </div>
+      `
+      
       })
-    }
-}
-
-
-
-})
-
-
-
-Vue.component('card', {
-   template: `
-<div class="card">
-
-<ul>
-               <li  v-for="t in tasks" >
-               {{t}}
-               <task @updateParent="onUpdateSalary" :name="t.name" :completed="t.completed">{{}}</task>
-               </li>
-</ul>
-        </div>
-
- `,
-props: {
-          count: Number,
-   },
-data() {
-       return {
-       tasks:[
-       {name:"task",completed:false},{name:"task1",completed:false},{name:"task2",completed:false}
-       ]
-       }
-   },
-methods: {
-    onUpdateSalary(data) {
-var dat = data.name
-console.log(dat)
-var myIndex = this.tasks.indexOf(dat);
-console.log(myIndex);
-if (myIndex !== -1) {
-
-    this.tasks.splice(myIndex, 1);
-}
-console.log(this.tasks)
-      this.tasks.push(data)
-    }
-   },
-computed: {
-       title() {
-       }
-}
-})
 
 
 let app = new Vue({
    el: '#app',
    data: {
-       premium: true,
-       cart: []
    },
 })
