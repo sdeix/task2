b6759f35f7920e51af7d2e26fd0fb2ce71b0ac55
sdeix <sdeix2765@gmail.com>
2023-02-08 11:22:32 +0700

    отображение даты в третьей колонке

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ template:`
       <div class="column column2">
       <card v-for="card in column2" :pointsAndTitle="card">  </card></div>
       <div class="column column3">
-      <card v-for="card in column3" :pointsAndTitle="card">  </card></div>
+      <card v-for="card in column3" :dat="dat" :pointsAndTitle="card" >  </card></div>
 
 
 
@@ -46,6 +46,7 @@ data(){
             column2:[],
             column3:[],
             blockOne:false,
+            dat: "",
       }
   },
 methods:{
@@ -115,6 +116,9 @@ mounted(){
                   }
 
                   if(this.column2[i].points.length === numbOfChecked){
+
+                      var now = new Date() 
+                      this.dat = now
                       this.column3.push(this.column2[i])
                       this.column2.splice(i, 1)
                       this.blockOne=false
@@ -139,9 +143,10 @@ Vue.component('card',{
                @click="check"
                :class="{done:point.checked}">
                
-              <li v-if="!block" >{{point.point}}</li>
+              <li :class=class3 v-if="!block" >{{point.point}}</li>
               <hr>
           </div>
+          <li>{{dat}}</li>
       </ul>
       </div>
       `,
@@ -151,7 +156,8 @@ Vue.component('card',{
       },
       props:{
             pointsAndTitle: null,
-            block: false
+            block: false,
+            dat: "",
 
       },
       methods: {
